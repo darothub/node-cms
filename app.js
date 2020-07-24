b3f3273e001242df7abbce6384c8a1121086dfda
darot <darotudeen@gmail.com>
2020-07-24 18:59:09 +0100

    connect to mongodb

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 import express from "express"
 import mongoose from "mongoose";
+import path from 'path'
+import hbs from 'express-handlebars'
 
 const app = express();
+var mongoDB = 'mongodb://127.0.0.1/my_cms_database'
+//Configure mongoDB
+mongoose.connect(mongoDB, { useNewUrlParser: true })
+.then(res => {
+    console.log("Mongod connected successfully")
+}).catch(err =>{
+    console.log("Connection failed")
+})
 
+app.engine('handlebars', hbs({defaultLayout: 'default'}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/',(req,res) => {
-    res.send("Hello Babel")
+    res.send("Hello CMS APP")
 })
 
 app.listen(4000,() => {
